1bf24c3b276734234a644d910497335bd7100aee
MJeremy2017 <zhangyue9306@gmail.com>
2019-01-16 17:43:27 +0800

    package

---
+++ src/scala4impatient/package.scala
@@ -0,0 +1,16 @@
+package object scala4impatient {
+  val defaultValue = 3
+
+}
+
+package scala4impatient {
+
+  class Person {
+    var name = defaultValue
+  }
+
+}
+
+// renaming
+import java.util.{HashMap => JavaHashMap}
+
