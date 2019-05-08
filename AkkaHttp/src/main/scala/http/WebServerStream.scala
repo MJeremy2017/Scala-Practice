2e08a81f4bb229eb144ee5f3c6d25fee0c0f1d68
MJeremy2017 <zhangyue9306@gmail.com>
2019-05-08 16:14:09 +0800

    add stream ws

---
+++ AkkaHttp/src/main/scala/http/WebServerStream.scala
@@ -0,0 +1,51 @@
+package http
+
+/**
+  * Handling streaming data
+  */
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl._
+import akka.util.ByteString
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpEntity, ContentTypes}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.util.Random
+import scala.io.StdIn
+
+
+object WebServerStream {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val numbers = Source.fromIterator(() =>
+      Iterator.continually(Random.nextInt()))
+
+    val route =
+      path("random") {
+        get {
+          complete(
+            HttpEntity(
+              ContentTypes.`text/plain(UTF-8)`,
+              // transform each number to a chunk of bytes
+              numbers.map(n => ByteString(s"$n\n"))  // continuously generating result
+            )
+          )
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)  // mar
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+
+  }
+
+}
