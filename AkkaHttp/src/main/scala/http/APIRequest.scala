6abe173c2755f1270c1524bb6138f1d1af6c3f24
MJeremy2017 <zhangyue9306@gmail.com>
2019-09-24 12:26:36 +0800

    add api request

--- AkkaHttp/src/main/scala/http/APIRequest.scala
+++ AkkaHttp/src/main/scala/http/APIRequest.scala
@@ -1,5 +1,50 @@
 package http
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+
+
 object APIRequest {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val records =
+      """{
+        |"records":
+        |[
+        |{
+        |"merchantID": "*",
+        |"unix_time": *,
+        |"cityId": *
+        |}
+        |]
+        |}""".stripMargin
+
+    val request = HttpRequest(
+      HttpMethods.POST,
+      uri = "http://localhost:8000/api/v1/predict/drm_rest_close?tag=v1",
+      entity = HttpEntity(ContentTypes.`application/json`, records)
+    )
+
+    val response: Future[HttpResponse] = Http().singleRequest(request)
+
+    response.onComplete {
+      case res: Success[HttpResponse] => println(res.get.status)
+      case Failure(_)   => sys.error("something wrong")
+    }
+
+    println(records)
+
+  }
 
 }
